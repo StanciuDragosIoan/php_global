01b30e22b140071f5ec9658f3dd772d38dca42fe
Dragos <Dragos.Dragos@honeywell.com>
2019-09-02 08:13:04 +0300

    test

--- index.php
+++ index.php
@@ -1,34 +1,52 @@
 <?php
- 
-//createa database cmd sql-> create database name ->create database mytodo;
-
-//use mytodo; (open the DB);  (CREATED A DB called todos);
-
-//once the DB opened, run -> show tables; (to see the tables)
-
-//create table todos (description text, completed boolean);    ->this creates the table (not how we specify the type of data to be put in the database entries)
-
-//describe todos; (shows details about DB);
-
-
-//drop table todos; (deletes the table);
-
-//create table todos (description text NOT NULL, completed boolean NOT NULL);  (we need to specify NOT NULL for values or we'll get an error); ->we do this so that we can make more empty rows in which to fill the description and completed (in this case);
-
-
-
-//create table todos (id integer PRIMARY KEY AUTO_INCREMENT, description text NOT NULL, completed boolean NOT NULL);  (added autoincrement id which is primary key);
-
-
-
-//insert into todos (description, completed) values('Go to the store', false);     (inserted 1st record into table)
-
-//select * from todos;  (shows all items in the todos table);
 
 require 'functions.php';
+ 
+ //CLASSES (e.g. Task, Comment, User, etc);
+
+class Task {
+    protected $description;
+
+    protected $completed = false;
+
+    public function __construct($description){
+        //automatically triggered on instantiation
+        $this->description = $description;
+    }
+
+    public function complete(){
+        $this->completed = true;
+    }
+
+    public function isComplete() {
+        return $this->completed;
+    }
+}
+
+
+$task = new Task('Go to the store');
+
+//$task->isComplete;
+
+// dd($task);
+
+$task->complete(); //complete the task
+
+var_dump($task->isComplete());
+
 require 'index.view.php';
 
+//public properties can be accessed from outside the class -> $task->description;
+
+//private properties we cannot access from outside the class -> $task->description;
 
 
+$tasks = [
+    new Task('Go to the store'),
+    new Task('Finish my screencast'),
+    new Task('Clean my room')
+];
+
+//dd($tasks);
 
 ?>
--- index.view.php
+++ index.view.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>my_sql</title>
 </head>
 <body>
 
-        <h1>
-            My_SQL
-        </h1>
+    <?php foreach($tasks as $task) : ?>
+        <li>
+            <?=$task->description;?>
+        </li>
+    <?php endforeach; ?>
 
        
 </body>
