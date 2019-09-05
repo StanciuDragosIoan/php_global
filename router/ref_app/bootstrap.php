6a3b004714eec36fd6a2241856f3d48fe77975a1
Dragos <Dragos.Dragos@honeywell.com>
2019-09-06 02:30:21 +0300

    test

--- router/ref_app/bootstrap.php
+++ router/ref_app/bootstrap.php
@@ -1,12 +1,8 @@
 <?php
 
 	$config = require 'config.php';
-
-	require 'core/Router.php';
-
-    require 'core/database/Connection.php';
-
-    require 'core/database/QueryBuilder.php';
+    require 'database/Connection.php';
+    require 'database/QueryBuilder.php';
 
     
    return new QueryBuilder(
---
+++ router/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+$router-> define([
+
+    'phprepo/router' => 'controllers/index.php',
+
+    'phprepo/router/about' => 'controllers/about.php',
+
+    'phprepo/router/about/culture' => 'controllers/about-culture.php',
+
+    'phprepo/router/contact' => 'controllers/contact.php'
+]);
